37612de0bbc4b4d844219be45ea671ad1d715a0a
Jahana Sherin tk <jahanasherintk3@gmail.com>
2023-09-26 23:07:27 +0530

    final

--- src/Component/RowPost/RowPost.js
+++ src/Component/RowPost/RowPost.js
@@ -1,53 +1,64 @@
-import React, { useEffect,useState } from 'react'
-import './RowPost.css'
-import axios from '../../axios'
-import {imageUrl,API_KEY} from '../../Constants/constants'
-import Youtube from 'react-youtube'
+import React, { useEffect, useState } from "react";
+import "./RowPost.css";
+import axios from "../../axios";
+import { imageUrl, API_KEY } from "../../Constants/constants";
+import Youtube from "react-youtube";
 
 const RowPost = (props) => {
-  const [movies,setMovies]=useState([])
-  const [urlId,setUrlId]= useState('')
-  useEffect(()=>{
-      axios.get(props.url).then(response=>{
-        console.log(response.data)
-        setMovies(response.data.results)
-      }).catch(err=>{
-
+  const [movies, setMovies] = useState([]);
+  const [urlId, setUrlId] = useState("");
+  useEffect(() => {
+    axios
+      .get(props.url)
+      .then((response) => {
+        console.log(response.data);
+        setMovies(response.data.results);
       })
-  },[])
+      .catch((err) => {});
+  }, []);
   const opts = {
-    height: '390',
-    width: '1500',
+    height: "390",
+    width: "1500",
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
   };
-  const handleMovie = (id)=>{
-    console.log(id)
+  const handleMovie = (id) => {
+    console.log(id);
 
-    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
-      if(response.data.results.length!==0){
-        setUrlId(response.data.results[0])
-      }else{
-        console.log('Array empty')
-      }
-    })
-  }
+    axios
+      .get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        if (response.data.results.length !== 0) {
+          setUrlId(response.data.results[0]);
+        } else {
+          console.log("Array empty");
+        }
+      })
+  };
+
   return (
+    <div className="row">
+      <h2>{props.title}</h2>
 
-    <div className='row'>
-        <h2>{props.title}</h2>
-        <div className='posters'>
-
-          {movies.map((obj)=>
-            <img onClick={()=>handleMovie(obj.id)}className={props.isSmall ? 'smallPoster':"poster"} src={`${imageUrl+obj.backdrop_path}`} alt='poster'></img>
-          )}
+      <div className="posters">
+          {movies.map((obj) => 
             
-        </div>
-       { urlId &&<Youtube opts={opts} videoId={urlId.key}/>}
+                <img
+                  onClick={() => handleMovie(obj.id)}
+                  className={props.isSmall ? "smallPoster" : "poster"}
+                  src={`${imageUrl + obj.backdrop_path}`}
+                  alt="poster"
+                />
+            
+          )}
+        
+      </div>
+      
+      {urlId && <Youtube opts={opts} videoId={urlId.key} />}
     </div>
-  )
-}
+  );
+};
 
-export default RowPost
+export default RowPost;
